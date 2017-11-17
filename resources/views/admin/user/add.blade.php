58982566187591ad046033a546dd1317227063d4
Phan Khoi <vothien10101996@gmail.com>
2017-11-17 14:15:26 +0700

    static pages admin

---
+++ resources/views/admin/user/add.blade.php
@@ -0,0 +1,8 @@
+@extends("admin.layouts.master")
+@section('title')
+	{{ trans('Add') }}
+@endsection
+
+@section('content')
+
+@endsection
---
+++ resources/views/admin/user/index.blade.php
@@ -0,0 +1,8 @@
+@extends("admin.layouts.master")
+@section('title')
+	{{ trans('List user') }}
+@endsection
+
+@section('content')
+
+@endsection
